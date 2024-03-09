31aee61c02a8fe526ab42ea1cdeeff22f4c4c8e9
AhmadyarRashid <ahmadyarmeo@gmail.com>
2024-03-09 18:50:53 +0500

    update instruction page

--- src/screens/instructions/index.js
+++ src/screens/instructions/index.js
@@ -30,53 +30,37 @@ function InstructionScreen() {
         <ScrollView>
           <View style={styles.main}>
             <Text style={styles.content}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Vestibulum sollicitudin erat ligula, tempus mollis sem sodales
-              nec. Nullam ullamcorper efficitur luctus. Vestibulum lobortis sit
-              amet nisl cursus vulputate. Morbi convallis lectus odio, a
-              ullamcorper arcu faucibus nec. Duis quis suscipit diam. Mauris
-              dictum ante purus, vel ultricies massa dignissim ac. Sed at sem
-              velit. Integer vel leo interdum, ultrices odio ac, rutrum massa.
-              Morbi commodo lectus magna, id malesuada dui hendrerit quis.
-              Praesent ac volutpat odio. Donec dapibus facilisis velit ut
-              commodo. Donec purus nisl, pharetra id mollis ut, convallis eu
-              leo. Nam tincidunt facilisis purus, sit amet eleifend sapien
-              condimentum et. Donec dapibus aliquet ante eget gravida.
-              Suspendisse nec ipsum libero. Morbi hendrerit non neque sit amet
-              rhoncus. Nam id quam laoreet, cursus quam at, tincidunt felis.
-              Nunc feugiat tincidunt ex, ac luctus nibh interdum et. Cras
-              pharetra ligula nisi, a dignissim nunc ultrices ac. Quisque
-              scelerisque ultricies est ac porttitor. Aliquam ut augue sed enim
-              vulputate ultrices nec eget elit. Nulla magna ante, accumsan non
-              erat eu, finibus tincidunt sapien. Vestibulum quis pretium neque.
-              Suspendisse quam lacus, ullamcorper id dolor vitae, semper
-              sollicitudin justo. Morbi sed mattis risus. Vivamus ut tempor
-              lorem, a rutrum lectus. Donec ut nunc ac mauris laoreet finibus.
-              Quisque non sapien blandit, iaculis lacus et, tempus lectus. Proin
-              imperdiet elit sed nibh imperdiet semper. Lorem ipsum dolor sit
-              amet, consectetur adipiscing elit. Morbi fringilla accumsan odio,
-              eget varius augue dictum sed. Nunc quis porttitor augue, vitae
-              faucibus ante. Vestibulum ut rutrum libero, id fermentum nulla.
-              Duis ac ante in nisi blandit lobortis at a massa. Duis faucibus
-              dui ac facilisis egestas. Fusce urna orci, lacinia eu leo vel,
-              feugiat viverra quam. Maecenas tellus nibh, vestibulum a maximus
-              vel, feugiat pulvinar nisl. Ut et rutrum justo. Integer semper
-              consectetur sem, vitae laoreet ipsum scelerisque nec. Quisque
-              felis neque, ultrices at lobortis ut, mattis pretium mi.
-              Vestibulum turpis dui, eleifend quis sapien vel, aliquet finibus
-              urna. urna. Viva-mus sodales, lectus non maximus mollis, dolor ex
-              gravida arcu, vitae viverra nisl nulla et neque. Etiam maximus sem
-              lacus, vel iaculis nunc egestas vel. Vestibulum dictum urna vel
-              libero posuere, a viverra massa placerat. Curabitur dictum urna et
-              congue tempor. In in purus consectetur, ornare purus et, dictum
-              libero. Mauris at augue eu felis bibendum vulputate. Donec
-              ultricies pulvinar elementum. Mauris vel risus id eros maximus
-              faucibus. Phasellus ullamcorper dignissim vulputate. Nulla id
-              vehicula enim, eu ultrices justo. Pellentesque mollis laoreet
-              justo non ullamcorper. Morbi molestie pulvinar nunc id cursus.
-              Cras sollicitudin luctus turpis in dignissim. Duis sollicitudin
-              accumsan nisl.
+              1. To find USC store please press{' '}
+              <Text style={styles.highlightedText}>Store Locator Button.</Text>
+              {'\n'}
+              {'\n'}
+              2. To search for your desired USC store apply{' '}
+              <Text style={styles.highlightedText}>
+                Filters. Zone, Region & Category.
+              </Text>
+              {'\n'}
+              {'\n'}
+              3. To open list of Subsidized Products press{' '}
+              <Text style={styles.highlightedText}>
+                Subsidized Products Button.
+              </Text>
+              {'\n'}
+              {'\n'}
+              4. To open the list of Other Products press{' '}
+              <Text style={styles.highlightedText}>Other Products Button.</Text>
             </Text>
+            <Text style={styles.urduTitle}>ہدایات</Text>
+            <Text style={styles.content}>
+              1. اسٹور تلاش کرنے کے لیے براہ کرم اسٹور لوکیٹر بٹن کو دبائیں ۔ 
+                2. اپنے مطلوبہ USC اسٹور کو تلاش کرنے کے لیے  فلٹرز لگائیں. زون،
+              علاقہ اور زمرہ ۔  3. سبسڈی والی مصنوعات کی فہرست کھولنے کے لیے
+              سبسڈی والی مصنوعات کا بٹن دبائیں ۔   4. دیگر مصنوعات کی فہرست کو
+              کھولنے کے لیے دیگر مصنوعات کا بٹن دبائیں .
+            </Text>
+            <TouchableOpacity style={styles.watchBtn}>
+              <Text style={styles.watchBtnText}>Watch Video Guide</Text>
+              <Text style={styles.watchBtnText}>ویڈیو گائیڈ دیکھیں</Text>
+            </TouchableOpacity>
           </View>
         </ScrollView>
       </ImageBackground>
@@ -111,7 +95,7 @@ const styles = StyleSheet.create({
   },
   main: {
     marginTop: '8%',
-    marginBottom: '8%',
+    marginBottom: '12%',
     paddingHorizontal: '8%',
     display: 'flex',
     flexDirection: 'column',
@@ -121,6 +105,28 @@ const styles = StyleSheet.create({
   content: {
     fontSize: 18,
     color: 'gray',
+    alignSelf: 'flex-end',
+  },
+  highlightedText: {
+    color: '#F48423',
+  },
+  urduTitle: {
+    marginTop: 20,
+    color: '#81BB50',
+    alignSelf: 'center',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  watchBtn: {
+    marginTop: '8%',
+    borderRadius: 22,
+    backgroundColor: '#F48423',
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+  },
+  watchBtnText: {
+    color: 'white',
+    alignSelf: 'center',
   },
 });
 
