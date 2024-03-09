1b11721837db56726e14743f7e5bb730a80a3b70
AhmadyarRashid <ahmadyarmeo@gmail.com>
2024-03-09 10:01:50 +0500

    add rough splash screen

--- src/screens/splash/index.js
+++ src/screens/splash/index.js
@@ -1,13 +1,60 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
 
 function SplashScreen() {
-  // return <Text>Splash Screen</Text>;
   return (
-    <View>
-      <Text>Splash Screen</Text>
+    <View style={styles.container}>
+      <View style={styles.logoContainer}>
+        <Image
+          source={require('./../../assets/logo.png')}
+          style={styles.logo}
+        />
+        <Text style={styles.title}>MY USC</Text>
+        <Image
+          source={require('./../../assets/basket.png')}
+          style={styles.shoppingCart}
+        />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logoContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 150,
+    height: 150,
+    resizeMode: 'contain',
+  },
+  title: {
+    color: '#F48323',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 20,
+  },
+  shoppingCartContainer: {
+    position: 'absolute',
+    bottom: 50,
+    alignItems: 'center',
+  },
+  shoppingCart: {
+    width: 250,
+    height: 250,
+    resizeMode: 'contain',
+  },
+  subtitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
 
 export default SplashScreen;
